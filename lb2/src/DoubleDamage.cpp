01018455131285425d3d689816cd6945e8d05ae3
Rodnov-Ivan <144445199+Rodnov-Ivan@users.noreply.github.com>
2024-11-08 00:55:20 +0300

    Delete lb2 directory

--- lb2/src/DoubleDamage.cpp
+++
@@ -1,9 +0,0 @@
-#include "DoubleDamage.hpp"
-
-DoubleDamage::DoubleDamage(GameField& field):field(field) {};
-
-
-void DoubleDamage::useAbility() {
-    std::cout << "Double Damage is applying." << std::endl;
-    field.setDoubleDamage();
-}
